9a94ffcd0b37ca19e8c84af29c3673dda0e47d0a
Shubham-1103 <s.sharma3625@gmail.com>
2020-12-17 02:56:07 +0530

    set up the authentication and autherization of user

---
+++ src/Player.js
@@ -0,0 +1,11 @@
+import React from  'react';
+
+function Player(){
+    return(
+        <div>
+            <h1>Player is here!! 🚀</h1>
+        </div>
+    )
+}
+
+export default Player;
---
+++ src/spotify.js
@@ -0,0 +1,26 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId =  "bb1dceaf856c41189e3845026b695738";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state"
+]
+
+
+export const getTokenFormUrl = () =>{
+    return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) => {
+        //#access_token=mysSuperSceretString&name=shubh
+        let parts= item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+        return initial;
+    }, {})
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
